edaffa6b22f4a7601bf0b81fdc868a8e662fde06
Aryan :) <aryansp13oct@gmail.com>
2026-06-28 12:13:46 +0530

    Time: 4 ms (34.9%), Space: 10 MB (11.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) 
+            return false;
+            
+        unordered_map<char, int> a;
+        unordered_map<char, int> b;
+        for (int i = 0; i < s.length(); i++) {
+            a[s[i]]++;
+            b[t[i]]++;
+        }
+        return a == b;
+    }
+};
